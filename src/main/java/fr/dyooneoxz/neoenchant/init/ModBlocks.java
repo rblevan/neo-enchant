6c99012041f12abde8139eaf3fa535e316923807
rblevan <ribouleau.evan@gmail.com>
2026-03-23 16:16:58 +0100

    works on primo obsidian

--- src/main/java/fr/dyooneoxz/neoenchant/init/ModBlocks.java
+++ src/main/java/fr/dyooneoxz/neoenchant/init/ModBlocks.java
@@ -2,6 +2,8 @@ package fr.dyooneoxz.neoenchant.init;
 
 import fr.dyooneoxz.neoenchant.NeoEnchant;
 import fr.dyooneoxz.neoenchant.block.PrimordialMonolithBlock;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -10,12 +12,27 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, NeoEnchant.MODID);
 
-    public static final RegistryObject<Block> PRIMORDIAL_MONOLITH = BLOCKS.register("primordial_monolith",
+    public static final RegistryObject<Block> PRIMORDIAL_MONOLITH = registerBlock("primordial_monolith",
             () -> new PrimordialMonolithBlock(BlockBehaviour.Properties.copy(Blocks.ENCHANTING_TABLE)));
+
+    public static final RegistryObject<Block> PRIMORDIAL_OBSIDIAN = registerBlock("primordial_obsidian.json",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OBSIDIAN)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
